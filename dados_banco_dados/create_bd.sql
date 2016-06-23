a71a5b178ba9743d4e88fd4d8fa41a7dac387f75
Julio Sieg <julio.sieg@gmail.com>
2016-06-23 10:25:21 -0300

    Dados do Banco de Dados adicionados
    Modelo Conceitual e SQL

---
+++ dados_banco_dados/create_bd.sql
@@ -0,0 +1,103 @@
+﻿create table categoria(
+id serial primary key,
+descricao varchar(100) not null
+)
+
+create table modelo(
+id serial primary key,
+descricao varchar(100) not null
+)
+
+create table marca(
+id serial primary key,
+descricao varchar(100) not null
+)
+
+create table destaques(
+id serial primary key,
+foto varchar(200) not null
+)
+
+create table entregas(
+id serial primary key,
+foto_destaque varchar(200) not null,
+foto_1 varchar(200),
+foto_2 varchar(200),
+foto_3 varchar(200),
+titulo varchar(100) not null,
+observacoes varchar(500) not null 
+)
+
+create table opcionais(
+id serial primary key,
+descricao varchar(100) not null
+)
+
+create table carro(
+id serial primary key not null,
+titulo varchar(200) not null,
+quilometragem varchar(50) not null,
+combustivel varchar(50) not null,
+cor varchar(50) not null,
+ano_modelo varchar(4) not null,
+ano_fabricacao varchar(4) not null,
+observacoes varchar(500),
+foto_1 varchar(200),
+foto_2 varchar(200),
+foto_3 varchar(200),
+foto_4 varchar(200),
+id_modelo integer not null,
+id_marca integer not null,
+id_categoria integer not null,
+FOREIGN KEY (id_modelo) references modelo(id),
+FOREIGN KEY (id_marca) references marca(id),
+FOREIGN KEY (id_categoria) references categoria(id)
+)
+
+create table carro_opcionais(
+id serial primary key,
+id_carro integer not null,
+id_opcionais integer not null,
+foreign key (id_carro) references carro(id),
+foreign key (id_opcionais) references opcionais(id)
+)
+
+create table interesse(
+id serial primary key,
+nome varchar(100) not null,
+email varchar(100),
+observacao varchar(500),
+telefone varchar(20) not null,
+id_carro integer not null,
+foreign key (id_carro) references carro(id)
+)
+
+create table perfil(
+id serial primary key,
+descricao varchar(100) not null
+)
+
+create table acesso(
+id serial primary key,
+funcao varchar(100) not null,
+tela varchar(100) not null
+)
+
+create table usuario(
+id serial primary key,
+login varchar(100) not null,
+senhar varchar(100) not null,
+nome varchar(200) not null,
+id_perfil integer not null,
+foreign key (id_perfil) references perfil(id)
+)
+
+create table perfil_acesso(
+id serial primary key,
+id_perfil integer not null,
+id_acesso integer not null,
+foreign key (id_perfil) references perfil(id),
+foreign key (id_acesso) references acesso(id)
+)
+
+
--- funcoes/funcoesDashboard.php
+++ funcoes/funcoesDashboard.php
@@ -13,12 +13,12 @@ switch($funcao){
 
 function getCounterFacebook(){
 
-	//Teste com página do Himundo, deve ser alterado para pegar valor correto
+	//Teste com p?gina do Himundo, deve ser alterado para pegar valor correto
 	//Para alterar, tem que alterar o valor da variavel fid
 	//Para pegar o id de uma pagina, da pra usar o seguinte site: http://findmyfbid.com/
-	//Exemplo - https://www.facebook.com/himundo = 770555776396053
+	//Exemplo - https://www.facebook.com/centraldoautomovelibiruba = 463898903818089
 
-	$fid = '770555776396053';
+	$fid = '463898903818089';
 
 	$url = 'http://api.facebook.com/method/fql.query?format=json&query=select+fan_count,name+from+page+where+page_id%3D'.$fid;
 
